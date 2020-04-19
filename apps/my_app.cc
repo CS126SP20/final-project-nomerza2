52c5e56128a9fdf8dc5fa574031554f1efe7e258
nomerza2 <crunchbaccaUVI13>
2020-04-19 14:31:07 -0500

    Added foot sensor and contact listener to player, to prevent the player from jumping when they are not on the ground.

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -5,6 +5,7 @@
 #include <cinder/app/App.h>
 #include "cinder/gl/gl.h"
 
+int sensor_contacts = 0; //TODO extern definition here should it be fixed?
 
 namespace myapp {
 
@@ -36,10 +37,21 @@ MyApp::MyApp() {
   fixtureDef.friction = 0.3f;
   body->CreateFixture(&fixtureDef);
 
-  //world_point = &world;
+  //Foot Sensor for jumping
+  b2PolygonShape sensor_box;
+  sensor_box.SetAsBox(0.9f, 0.1f, b2Vec2(0, -1), 0); //x is almost same as player, smaller to prevent jumping when leaning against wall. y is much smaller center in y must equal the negative half-height of the player
+  b2FixtureDef sensor_fixture_def;
+  sensor_fixture_def.shape = &sensor_box;
+  sensor_fixture_def.isSensor = true;
+  b2Fixture* sensor_fixture = body->CreateFixture(&sensor_fixture_def);
+  sensor_fixture->SetUserData((void*)3); //Not sure what this syntax does, but gives it tag of "3". Maybe use constant instead?
+
+  //Set Up the Foot Sensor Listener
+  world->SetContactListener(&contactListener);
 }
 
 void MyApp::setup() {
+
 }
 
 void MyApp::update() {
@@ -65,23 +77,43 @@ void MyApp::draw() {
 }
 
 void MyApp::keyDown(KeyEvent event) {
-    if (event.getCode() == KeyEvent::KEY_UP) { //TODO take event.getCode() out of conditional?
+    if (event.getCode() == KeyEvent::KEY_UP && sensor_contacts >= 1) {  // only jump if in contact with ground //TODO take event.getCode() out of conditional?
         b2Vec2 impulse_vector(0.0f, 42.0f);//Arbitrarily chosen value, looks good in testing.
         body->ApplyLinearImpulse(impulse_vector, body->GetPosition());
+
     } else if (event.getCode() == KeyEvent::KEY_RIGHT) {
-        b2Vec2 velocity(5.0f, 0.0f);//TODO take velocity changes into helper function?
+        b2Vec2 velocity(5.0f, body->GetLinearVelocity().y); // Need to use previous y velocity, or trying to move side to side mid-air will cause player to suddenly fall
+        // TODO take velocity changes into helper function?
         body->SetLinearVelocity(velocity);
     } else if (event.getCode() == KeyEvent::KEY_LEFT) {
-        b2Vec2 velocity(-5.0f, 0.0f);
+        b2Vec2 velocity(-5.0f, body->GetLinearVelocity().y);
         body->SetLinearVelocity(velocity);
     }
 }
 
 void MyApp::keyUp(cinder::app::KeyEvent event) {
     if (event.getCode() == KeyEvent::KEY_LEFT || event.getCode() == KeyEvent::KEY_RIGHT) {
-        b2Vec2 velocity(0.0f, 0.0f);
+        b2Vec2 velocity(0.0f, body->GetLinearVelocity().y);
         body->SetLinearVelocity(velocity);
     }
 }
 
+
+// Code for ContactListeners to determine if the user is able to jump (citation):
+// https://www.iforce2d.net/b2dtut/jumpability
+// Code has been modified to avoid repetition
+
+void MyApp::ContactListener::BeginContact(b2Contact* contact) {
+    //check if either fixture is the foot sensor
+    if ((int)contact->GetFixtureA()->GetUserData() == 3 || (int)contact->GetFixtureB()->GetUserData() == 3) {
+        sensor_contacts++;
+    }
+}
+
+void MyApp::ContactListener::EndContact(b2Contact* contact) {
+    //check if either fixture is the foot sensor
+    if ((int)contact->GetFixtureA()->GetUserData() == 3 || (int)contact->GetFixtureB()->GetUserData() == 3) {
+        sensor_contacts--;
+    }
+}
 }  // namespace myapp
--- apps/my_app.h
+++ apps/my_app.h
@@ -6,6 +6,7 @@
 #include <cinder/app/App.h>
 #include <Box2D/Box2D.h>
 
+extern int sensor_contacts; //TODO is global ok? and should it be in this file?
 
 namespace myapp {
 
@@ -18,11 +19,25 @@ class MyApp : public cinder::app::App {
   void keyDown(cinder::app::KeyEvent) override;
   void keyUp(cinder::app::KeyEvent) override;
 
+ protected:
+  //TODO better name for sensor_contacts?
+  //int sensor_contacts; // TODO is making this static ok / will it cause problems
 
  private:
   b2World* world;
   b2Body* body;
+
+  //This is a declaration for the subclass
+  class ContactListener : public b2ContactListener { //TODO see if it is ok to have 2 classes in one file (OR subclass 'cause I guess that's what I'm doing)?
+    void BeginContact(b2Contact* contact);
+    void EndContact(b2Contact* contact);
+  };
+
+  //This is a private member variable of MyApp
+  ContactListener contactListener;
+
 };
+
 }  // namespace myapp
 
 #endif  // FINALPROJECT_APPS_MYAPP_H_
