2eeeb2342dc99b7dcc54872db68547fd049694ab
nomerza2 <crunchbaccaUVI13>
2020-04-30 13:49:56 -0500

    Created Wall Class to allow generic creation of any static box to be used in the game. Contains constructor and Draw(). Unimplemented.

---
+++ include/mylibrary/Wall.h
@@ -0,0 +1,27 @@
+//
+// Created by natha on 4/30/2020.
+//
+
+#ifndef FINALPROJECT_WALL_H
+#define FINALPROJECT_WALL_H
+
+#include <Box2D/Box2D.h>
+#include <cinder/gl/gl.h>
+
+namespace mylibrary {
+
+class Wall {
+ public:
+  Wall(b2World* world, float x_loc, float y_loc, float half_width, float half_height, ci::Color color);
+  void Draw();
+
+ private:
+  float half_width_;
+  float half_height_;
+  b2Body* body_;
+  ci::Color color_;
+};
+
+}
+
+#endif  // FINALPROJECT_WALL_H
---
+++ src/Wall.cpp
@@ -0,0 +1,37 @@
+//
+// Created by natha on 4/30/2020.
+//
+
+#include "mylibrary/Wall.h"
+#include <cinder/app/App.h>
+
+const int kPixelsPerMeter = 90; //TODO Create unified declaration
+
+namespace mylibrary {
+
+Wall::Wall(b2World* world, float x_loc, float y_loc, float half_width, float half_height, ci::Color color) {
+  b2BodyDef bodyDef;
+  bodyDef.position.Set(x_loc, y_loc);
+  body_ = world->CreateBody(&bodyDef);
+  b2PolygonShape box;
+  box.SetAsBox(half_width, half_height);
+  body_->CreateFixture(&box, 0);
+
+  half_width_ = half_width;
+  half_height_ = half_height;
+  color_ = color;
+}
+
+void Wall::Draw() {
+  b2Vec2 position = body_->GetPosition();
+  ci::Rectf rect(
+      (position.x - half_width_)*kPixelsPerMeter,
+      ci::app::getWindowHeight() - ((position.y - half_height_) * kPixelsPerMeter),
+      (position.x + half_width_) * kPixelsPerMeter,
+      ci::app::getWindowHeight() - ((position.y + half_height_) * kPixelsPerMeter));
+
+  ci::gl::color(color_);
+  ci::gl::drawSolidRect(rect);
+}
+
+}
